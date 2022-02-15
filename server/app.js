65b3e432fcf5f6e75b627c97e0c31452bbdb5e64
Joseph <joseph@hassell.dev>
2022-02-15 11:56:02 -0600

    Adding flutter support

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+app.use('/apiv1', require('./apiv1/apiv1'));
+
+module.exports = app;
