8e636282b476bbc2bca86150078d90448a58d7a9
Joseph <joseph@hassell.dev>
2022-02-18 11:05:36 -0600

    s3 working

--- server/routes/apiv1/mediaAPI.js
+++ server/routes/apiv1/mediaAPI.js
@@ -9,12 +9,12 @@ const SMCloudStore = require('smcloudstore')
 // Complete with the connection options for GenericS3
 
 const connection = {
-    endPoint: process.env.S3_FULL_URL,
+    endPoint: process.env.S3_ENDPOINT,
     accessKey: process.env.S3_KEY_ID,
     secretKey: process.env.S3_APPLICATION_KEY,
-    region: process.env.S3_REGION
+    region: process.env.S3_REGION,
+    pathStyle: (process.env.S3_PATH_STYLE.toLowerCase()==='true')?true:false
 }
-
 // Return an instance of the GenericS3Provider class
 const storage = SMCloudStore.Create('generic-s3', connection)
 
@@ -26,13 +26,24 @@ function getS3SignatureKey(key, dateStamp, regionName, serviceName) {
     return kSigning;
 }
 
-router.get("/s3/new/*", async (req,res,next) => {
+router.get("/s3/*", async (req,res,next) => {
   try {
+    //let metaData = await storage.h(process.env.S3_BUCKET, req.params[0])
     console.log(req.params[0])
     let stream = await storage.getObject(process.env.S3_BUCKET, req.params[0])
-    console.log(stream)
-    res.write(stream)
-    res.end()
+    stream.on('error', function error(err) {
+        //continue to the next middlewares
+        return next(err);
+    });
+    //stream
+    console.log(stream.headers)
+    res.set('Content-Type', stream.headers['content-type']);
+    res.set('Content-Length', stream.headers['content-length']);
+    res.set('Last-Modified', stream.headers['last-modified']);
+    res.set('ETag', stream.headers['etag']);
+    res.set('Cache-Control', 'private, max-age=604800')
+    stream.pipe(res)
+    //res.end()
   } catch(e) {
     console.error(e)
     if(e?.code == 'NoSuchKey') {
@@ -48,24 +59,24 @@ router.get("/s3/new/*", async (req,res,next) => {
 });
 
 
-router.use('/s3/*', proxy(process.env.S3_FULL_URL, { //process.env.S3_FULL_URL
-  proxyReqPathResolver: function (req) {
-    console.log(req.params)
-    return req.url + req.params[0]
-  },
-  //proxyReqPathResolver: req => url.parse(req.baseUrl).path,
-  proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
-    //proxyReqOpts.headers = {"Authorization": `AWS ${process.env.S3_KEY_ID}:${getS3SignatureKey(process.env.S3_APPLICATION_KEY, Date.now())}`};
-    return proxyReqOpts;
-  }
-  // userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
-  //   // recieves an Object of headers, returns an Object of headers.
-  //   console.log(headers)
-  //   proxyReq.set("Authorization", `AWS AWSAccessKeyId:Signature`)
-  //   //console.log(proxyReq, )
-  //   return headers;
-  // }
-}));
+// router.use('/s3/*', proxy(process.env.S3_FULL_URL, { //process.env.S3_FULL_URL
+//   proxyReqPathResolver: function (req) {
+//     console.log(req.params)
+//     return req.url + req.params[0]
+//   },
+//   //proxyReqPathResolver: req => url.parse(req.baseUrl).path,
+//   proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+//     //proxyReqOpts.headers = {"Authorization": `AWS ${process.env.S3_KEY_ID}:${getS3SignatureKey(process.env.S3_APPLICATION_KEY, Date.now())}`};
+//     return proxyReqOpts;
+//   }
+//   // userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+//   //   // recieves an Object of headers, returns an Object of headers.
+//   //   console.log(headers)
+//   //   proxyReq.set("Authorization", `AWS AWSAccessKeyId:Signature`)
+//   //   //console.log(proxyReq, )
+//   //   return headers;
+//   // }
+// }));
 
 
 
